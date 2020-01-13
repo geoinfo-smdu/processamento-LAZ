befab83f6f1c08beb3e90c5bdaaadd7ce7ac9e6e
Fernando Gomes <feromes@gmail.com>
2020-01-13 13:51:57 -0300

    env

--- LasTools/processamento-HAG.ps1
+++ LasTools/processamento-HAG.ps1
@@ -1,7 +1,7 @@
 . .\env.ps1
 
 foreach ($item in (Get-Item $inputFolder).FullName) {
-    #lasheight -i $item  -store_as_extra_bytes -odir "\\SMDUGBC88\Compartilhada\Silvio\AltaEdif\MDS-LAZ\norm\" -odix "_norm" -olaz
+    #lasheight -i $item  -store_as_extra_bytes -odir ".\norm\" -odix "_norm" -olaz
     echo "*************************************************"
     echo $item
     echo "**************************************************"
