2913028bd11629ad080287d13224fa210c77e9f7
Fernando Gomes <feromes@gmail.com>
2020-01-13 15:19:42 -0300

    Adicionando arquivos MDS e MDT

---
+++ Gerar-ListaDeArquivos.ps1
@@ -0,0 +1,26 @@
+. .\env.ps1
+$campos = "SCM,FullPath,Name"
+
+echo $campos > ".\arquivos\MDSs.csv"
+
+$mdss = Get-ChildItem -Path $source_mdss -Filter "MDS*.laz" -Recurse -Depth 10000
+
+foreach ($mds in $mdss) {
+    [array]$f = $mds.name.split("_")[1],
+    $mds.FullName,
+    $mds.name
+    [string]$linha = $f -join ","
+    Add-Content -Path ".\arquivos\MDSs.csv" -Value $linha
+}
+
+echo $campos > ".\arquivos\MDTs.csv"
+
+$mdts = Get-ChildItem -Path $source_mdss -Filter "MDT*.laz" -Recurse -Depth 10000
+
+foreach ($mdt in $mdts) {
+    [array]$f = $mdt.name.split("_")[1],
+    $mdt.FullName,
+    $mdt.name
+    [string]$linha = $f -join ","
+    Add-Content -Path ".\arquivos\MDTs.csv" -Value $linha
+}
